9f556234f235feb4d507575b1bf813a03323ac24
Nicolás Poore <nicolaspoore@gmail.com>
2020-12-18 21:12:37 -0300

    Project working

---
+++ app.js
@@ -0,0 +1,121 @@
+/* Database config */
+
+require('dotenv').config();
+mongoose = require('mongoose')
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).
+  catch(err => console.log(err))
+
+mongoose.connection.on('error', err => {
+  logError(err)
+})
+
+/* Model config */
+
+const { Schema } = mongoose;
+const { ObjectId } = Schema
+
+const userSchema = new Schema({
+  username: {type: String, required: true}
+})
+
+const exerciseSchema = new Schema({
+  userId: {type: ObjectId, required: true},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: Date
+})
+
+const User = mongoose.model('User', userSchema, 'users')
+const Exercise = mongoose.model('Exercise', exerciseSchema, 'exercises')
+
+/* Model functions */
+
+// Users
+
+const createAndSaveUser = username => {
+  let user = new User({username: username})
+  return user.save()
+}
+
+const findUserById = userId => User.findById({ _id: userId }).exec()
+
+const showAllUsers = () => User.find().exec()
+
+const checkUsername = async username => {
+  const takenUsername = await User.findOne({ username: username })
+  if (takenUsername) return true
+  return false
+}
+
+const formatUser = full_object => finalResponse = { _id: full_object._id, username: full_object.username }
+
+// Exercises
+
+const createAndSaveExercise = (userId, desc, duration, date) => {
+  let exercise = new Exercise({
+    userId: userId,
+    description: desc,
+    duration: duration,
+    date: date
+  })
+  return exercise.save()
+}
+
+const formatExercise = async full_object => {
+  const userObject = await findUserById(full_object.userId)
+  const username = userObject.username
+  return finalResponse = {
+    _id: full_object.userId,
+    username: username,
+    date: full_object.date,
+    duration: full_object.duration,
+    description: full_object.description
+  }
+}
+
+//const createUserLog = (userId) => Exercise.find({ userId: userId }).select('-userId').exec()
+
+const createUserLog = (userId, to, from, limit) => {
+
+  to = to ? to : new Date(8640000000000000)
+  from = from ? from : new Date(-8640000000000000)
+
+  const query = Exercise.find({ userId: userId, date: { $gte: from, $lte: to } }).select('-userId')
+
+  if (limit) return query.limit(limit)
+  return query
+}
+
+const mapLog = log => {
+  return log.map(exer => {
+    return {
+      description: exer.description,
+      duration: exer.duration,
+      date: exer.date,
+    }
+  })
+}
+
+const formatLog = async (log, userId) => {
+  const userObject = await findUserById(userId)
+  const username = userObject.username
+  const count = log.length
+
+  return finalResponse = {
+    _id: userId,
+    username: username,
+    count: count,
+    log: mapLog(log)
+  }
+}
+
+/* Exports */
+
+exports.createAndSaveUser = createAndSaveUser
+exports.showAllUsers = showAllUsers
+exports.checkUsername = checkUsername
+exports.formatUser = formatUser
+exports.createAndSaveExercise = createAndSaveExercise
+exports.formatExercise = formatExercise
+exports.createUserLog = createUserLog
+exports.formatLog = formatLog
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const bodyParser = require('body-parser')
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,9 +10,58 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
+const createAndSaveUser = require('./app.js').createAndSaveUser
+const showAllUsers = require('./app.js').showAllUsers
+const checkUsername = require('./app.js').checkUsername
+const formatUser = require('./app.js').formatUser
+const createAndSaveExercise = require('./app.js').createAndSaveExercise
+const createUserLog = require('./app.js').createUserLog
+const formatExercise = require('./app.js').formatExercise
+const formatLog = require('./app.js').formatLog
 
+app.post('/api/exercise/new-user', async (req, res) => {
+  const username = req.body.username
+  if (await checkUsername(username)) return res.send('Username already taken')
 
+  const createdUser = await createAndSaveUser(username)
+  return res.json(formatUser(createdUser))
+})
+
+app.get('/api/exercise/users', async (req, res) => {
+  const allUsers = await showAllUsers()
+  return res.json(allUsers)
+})
+
+app.post('/api/exercise/add', async (req, res) => {
+  const userId = req.body.userId
+  const desc = req.body.description
+  const duration = req.body.duration
+  let date = req.body.date
+  
+  if (!date) {
+    date = new Date()
+  }
+  
+  const createdExercise = await createAndSaveExercise(userId, desc, duration, date)
+
+  return res.json(await formatExercise(createdExercise))
+
+})
+
+app.get('/api/exercise/log', async (req, res) => {
+  const userId = req.query.userId
+  const to = req.query.to
+  const from = req.query.from
+  const limit = parseInt(req.query.limit)
+
+  const userLog = await createUserLog(userId, to, from, limit)
+
+  return res.json(await formatLog(userLog, userId))
+
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
